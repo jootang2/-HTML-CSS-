beea7f06bbd5226a8f95507551eb34477d5a896c
jootang2 <joohwan.song15b@gmail.com>
2022-12-14 19:19:54 +0900

    :tada: first init-set liveReload

---
+++ src/main/java/com/study/firstBook/FirstBookApplication.java
@@ -0,0 +1,13 @@
+package com.study.firstBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/firstBook/FirstBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.firstBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
